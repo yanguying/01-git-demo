c2a96863cd92c8db49c03e4dc679fb629c6ad83e
lu <qiaotun@qq.com>
2020-08-12 10:40:10 +0800

    first commit locat repository

---
+++ 01-maven-git-demo/src/main/java/com/yu/mave/demp.java
@@ -0,0 +1,15 @@
+package com.yu.mave;
+
+/**
+ * 于潞
+ * 2020/8/12
+ */
+public class demp {
+
+    public static void main(String[] args) {
+
+        System.out.println("GIT IDEA整合");
+
+    }
+
+}
